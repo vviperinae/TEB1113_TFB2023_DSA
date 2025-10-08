5f5e7d961b434ad65467be5e1b3a8a8122401258
Safa <safasarfraz12@gmail.com>
2025-10-08 20:57:31 +0800

    initial commit

---
+++ task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Student {
+int id;
+string name;
+string email;
+};
+int main() {
+int opt = 1;
+int num;
+while (opt != 2) {
+cout << "Enter number of students: ";
+cin >> num;
+Student student[num];
+for (int i = 0; i < num; i++) {
+cout << "\nStudent " << i + 1 << " Details:\n";
+cout << "Enter name: ";
+cin >> student[i].name;
+cout << "Enter id: ";
+cin >> student[i].id;
+cout << "Enter email: ";
+cin >> student[i].email;
+}
+for (int i =0; i <num; i++){
+cout << "\nStudent " << i + 1 << " Details:\n";
+cout << "Name: " << student[i].name << "\n";
+cout << "ID: " << student[i].id << "\n";
+cout << "Email: " << student[i].email << "\n\n";
+}
+cout << "Continue? (1 FOR YES, 2 FOR NO): ";
+cin >> opt;
+}
+return 0;
+}
